0f94b5d7c2355563ee050613b29c5f9265f9a468
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-11-26 19:37:44 -0600

    building a stacked deck function

--- main.ts
+++ main.ts
@@ -10,6 +10,18 @@ function Shuffle_Deck () {
         Deck.push(Draw)
     }
 }
+function Stack_Deck () {
+    Deck = []
+    for (let value of Suits) {
+        for (let Face of DCHS) {
+            Deck.push("" + Face + value)
+        }
+    }
+    Discard = []
+}
+input.onButtonPressed(Button.AB, function () {
+    Stack_Deck()
+})
 input.onButtonPressed(Button.B, function () {
     CreateDeck()
 })
@@ -28,9 +40,11 @@ function CreateDeck () {
 let Deck: string[] = []
 let Draw = ""
 let Discard: string[] = []
+let DCHS: string[] = []
 let Card: string[] = []
 let Suits: string[] = []
 Suits = ["D", "C", "H", "S"]
 Card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+DCHS = ["A", "4", "7", "10", "K", "3", "6", "9", "Q", "2", "5", "8", "J"]
 CreateDeck()
 Discard = []
