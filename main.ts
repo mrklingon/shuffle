f82cf2608014b1c0f7c2ac4d0be32546c260bd4f
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-12-05 15:25:09 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -35,9 +35,6 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
     Shuffle_Deck()
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Stack_Deck()
-})
 function Show_Shuffle () {
     for (let index = 0; index < 2; index++) {
         basic.showLeds(`
